19d7ee05309a9d844971c353c64560b6fd534805
fharavena <fharavena@gmail.com>
2019-01-17 02:07:15 -0300

    Primer commit

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers')
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'))
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    let salida = {
-        nombre: 'francisco',
-        edad: 33,
-        url: req.url
-    }
-    res.send(salida);
-
+    res.render('home', {
+        nombre: 'franCISco'
+    });
 });
 
-app.get('/data', (req, res) => {
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/', (req, res) => {
     res.send('Hola Data');
-
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 })
